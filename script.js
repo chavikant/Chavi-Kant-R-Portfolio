b83ea2de0babb34c9181f8d27ae7dfd95960ea68
chavi kant .R <162958666+chavikant@users.noreply.github.com>
2025-03-22 22:54:28 +0530

    second

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll("nav ul li a");
+    links.forEach(link => {
+        link.addEventListener("click", function(e) {
+            if (!this.getAttribute("href").endsWith(".html")) {
+                e.preventDefault();
+            }
+
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+            window.scrollTo({
+                top: targetElement.offsetTop - 50,
+                behavior: "smooth"
+            });
+        });
+    });
+});
