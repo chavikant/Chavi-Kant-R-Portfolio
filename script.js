e24c434a86feb12e15ab09133d25ca3c495ffc3a
chavi kant .R <162958666+chavikant@users.noreply.github.com>
2025-03-23 15:33:44 +0530

    changes in the menu bar2

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const links = document.querySelectorAll("nav ul li a");
-    const toggleButton = document.getElementById("nav-toggle");
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("nav-toggle");
     const navLinks = document.querySelector("nav ul");
 
-    links.forEach(link => {
-        link.addEventListener("click", function(e) {
-            if (!this.getAttribute("href").endsWith(".html")) {
-                e.preventDefault();
-            }
-
-            const targetId = this.getAttribute("href").substring(1);
-            const targetElement = document.getElementById(targetId);
-            window.scrollTo({
-                top: targetElement.offsetTop - 50,
-                behavior: "smooth"
-            });
-        });
-    });
-
-    toggleButton.addEventListener("click", function() {
+    menuToggle.addEventListener("click", function () {
         navLinks.classList.toggle("active");
     });
 });
