785987117d19e1f13de61bc9939a6e9a6cbdd874
chavi kant .R <162958666+chavikant@users.noreply.github.com>
2025-03-23 16:21:47 +0530

    new animnation and alignmet

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.getElementById("nav-toggle");
-    const navLinks = document.querySelector("nav ul");
+    const navLinks = document.querySelector(".nav-links");
 
     menuToggle.addEventListener("click", function () {
         navLinks.classList.toggle("active");
